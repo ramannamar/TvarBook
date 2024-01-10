8d59447d587a2c5ce166886e60fce34021c32d57
raman162 <amazingbublik162@gmail.com>
2024-01-11 02:27:17 +0300

    added urls Follower

--- pynet/profiles/routers.py
+++ pynet/profiles/routers.py
@@ -23,6 +23,8 @@ urlpatterns = [
     path('swagger(?P<format>\.json|\.yaml)', schema_view.without_ui(cache_timeout=0), name='schema-json'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('follower/', include('src.followers.urls')),
+    path('wall/', include('src.wall.urls')),
 ]
 
 
